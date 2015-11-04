9b60f9a0315cd773a2c307f24fb62a8b17e86c65
Marius Vollen <mariusvollen@hotmail.com>
2015-11-04 10:54:15 +0100

    Initial commit 2

---
+++ src/tester/TestKlasse.java
@@ -0,0 +1,16 @@
+package tester;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+
+public class TestKlasse {
+
+
+	@Test
+	public void DFS() {
+		
+		assertEquals("LOL", "LOL");
+		
+	}
+}
