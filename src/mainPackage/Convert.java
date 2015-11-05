f460b39096f5c73af09333f8ba1ec1ed367d70f7
Marius Vollen <mariusvollen@hotmail.com>
2015-11-05 13:07:39 +0100

    Finished converter classes and tests

--- src/mainPackage/Convert.java
+++ src/mainPackage/Convert.java
@@ -1,5 +1,138 @@
 package mainPackage;
 
 public class Convert {
+	public static int HexToDec(String s) {
+		if (s.length()>6) {
+			throw new IllegalArgumentException("Argument larger than max limit");
+		}
+		int res = 0;
+		for (int i = 0; i < s.length(); i++) {
+			int value = s.charAt(s.length()-i-1);
+			switch (value) {
+			case 48:
+				value =0;
+				break;
+				
+			case 49:
+				value =1;
+				break;
+				
+			case 50:
+				value =2;
+				break;
+				
+			case 51:
+				value =3;
+				break;
+				
+			case 52:
+				value =4;
+				break;
+				
+			case 53:
+				value =5;
+				break;
+				
+			case 54:
+				value =6;
+				break;
+				
+			case 55:
+				value =7;
+				break;
+				
+			case 56:
+				value =8;
+				break;
+				
+			case 57:
+				value =9;
+				break;
+				
+			case 65:
+				value =10;
+				break;
 
+			case 66:
+				value =11;
+				break;
+
+			case 67:
+				value =12;
+				break;
+
+			case 68:
+				value =13;
+				break;
+
+			case 69:
+				value =14;
+				break;
+
+			case 70:
+				value =15;
+				break;
+
+			default:
+				throw new IllegalArgumentException("Illegal characters in the argument.");
+			}
+			res += value*Math.pow(16, (i));
+		}
+		return res;
+	}
+	public static String DecToHex(int d) {
+		if (d>16777215) {
+			throw new IllegalArgumentException("Argument larger than max limit");
+		}
+		int rem;
+		String res = "";
+		char[] hex = new char[]{'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+		while (d>0){
+			rem=d%16;
+			res=hex[rem]+res;
+			d=d/16;
+		}
+		return res;
+	}
+	public static String DecToBin(int d) {
+		int bit24 = (int) Math.pow(2, 24);
+		if (d>bit24) {
+			throw new IllegalArgumentException("Argument larger than max limit");
+		}
+		if (d == 0) {
+			return "0";
+		}
+		int i = 0;
+		for (; i < 24; i++) {
+			if (d<Math.pow(2, i)) {
+				break;
+			}
+		}
+		String res = "";
+		for (; i > 0; i--) {
+			if (d>=Math.pow(2, i-1)) {
+				d-=Math.pow(2, i-1);
+				res+= 1;
+			}else {
+				res+= 0;
+			}
+		}
+		if(d!=0){
+			System.err.println("'d' was not 0, and unexpected algorythm has occurred. " + d);
+		}
+		return res;
+	}
+	public static int BinToDec(String s) {
+		if (s.length()>24) {
+			throw new IllegalArgumentException("Argument larger than max limit");
+		}
+		int res = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if ((s.charAt(i)!='0')&&(s.charAt(i)!='1')) {
+				throw new IllegalArgumentException("Illegal characters in the argument.");
+			}
+			res += ((s.charAt(s.length()-i-1))-48)*Math.pow(2, (i));
+		}
+		return res;
+	}
 }
--- src/tester/TestKlasse.java
+++ src/tester/TestKlasse.java
@@ -1,16 +1,94 @@
 package tester;
 
 import static org.junit.Assert.*;
-import org.junit.*;
 
+import org.junit.*;
+import mainPackage.*;
 
 public class TestKlasse {
-
-
 	@Test
-	public void DFS() {
+	public void TestHexToDec() {
+		assertEquals(15, Convert.HexToDec("F"));
+		assertEquals(10, Convert.HexToDec("A"));
+		assertEquals(100, Convert.HexToDec("64"));
+		assertEquals(1000, Convert.HexToDec("3E8"));
+		assertEquals(12345, Convert.HexToDec("3039"));
+		assertEquals(99999, Convert.HexToDec("1869F"));
+		assertEquals(957103, Convert.HexToDec("E9AAF"));
+		assertEquals(817374, Convert.HexToDec("C78DE"));
+		assertEquals(182837, Convert.HexToDec("2CA35"));
+		assertEquals(15432098, Convert.HexToDec("EB79A2"));
 		
-		assertEquals("LOL", "LOL");
+		
+	}
+	@Test
+	public void TestDecToHex() {
+		assertEquals("F", Convert.DecToHex(15));
+		assertEquals("A", Convert.DecToHex(10));
+		assertEquals("64", Convert.DecToHex(100));
+		assertEquals("3E8", Convert.DecToHex(1000));
+		assertEquals("3039", Convert.DecToHex(12345));
+		assertEquals("1869F", Convert.DecToHex(99999));
+		assertEquals("E9AAF", Convert.DecToHex(957103));
+		assertEquals("C78DE", Convert.DecToHex(817374));
+		assertEquals("2CA35", Convert.DecToHex(182837));
+		assertEquals("EB79A2", Convert.DecToHex(15432098));
+		
+	}
+	@Test
+	public void TestDecToBin() {
+		assertEquals("101", Convert.DecToBin(5));
+		assertEquals("111", Convert.DecToBin(7));
+		assertEquals("0", Convert.DecToBin(0));
+		assertEquals("10000", Convert.DecToBin(16));
+		assertEquals("1100100", Convert.DecToBin(100));
+		assertEquals("11000000111001", Convert.DecToBin(12345));
+		assertEquals("11000011010011111", Convert.DecToBin(99999));
+		assertEquals("111010110111100110100010", Convert.DecToBin(15432098));
+		assertEquals("111111111111111111111111", Convert.DecToBin(16777215));
+	}
+	@Test
+	public void TestBinToDec() {
+		assertEquals(5, Convert.BinToDec("101"));
+		assertEquals(7, Convert.BinToDec("111"));
+		assertEquals(0, Convert.BinToDec("0"));
+		assertEquals(16, Convert.BinToDec("00010000"));
+		assertEquals(16, Convert.BinToDec("10000"));
+		assertEquals(100, Convert.BinToDec("1100100"));
+		assertEquals(12345, Convert.BinToDec("11000000111001"));
+		assertEquals(99999, Convert.BinToDec("11000011010011111"));
+		assertEquals(15432098, Convert.BinToDec("111010110111100110100010"));
+		assertEquals(16777215, Convert.BinToDec("111111111111111111111111"));
+		
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void TestDecToBinToLargeInt() {
+		Convert.DecToBin(2147483647);
+		
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void TestDecToHexToLargeInt() {
+		Convert.DecToHex(2147483647);
+		
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void TestHexToDecToLargeInt() {
+		Convert.HexToDec("FFFFFFFFFFFFFF");
+		
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void TestBinToDecToLargeInt() {
+		Convert.BinToDec("1111111111111111111111111");
+		
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void TestHexToDecIllegalCharacters() {
+		Convert.HexToDec("ØÆÅ");
+		
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void TestBinToDecIllegalCharacters() {
+		Convert.BinToDec("11118131111");
 		
 	}
 }
